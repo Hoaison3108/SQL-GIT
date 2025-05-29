4d16a26f13dfdd5d873b03eff6c55bc906e5b16c
Hoaison3108 <hoaison3108@gmail.com>
2025-05-29 16:33:01 +0700

    Update 3

---
+++ 10.Order By.sql
@@ -0,0 +1,39 @@
+﻿--Bài 10: ORDER BY sắp xếp kết quả trả về của truy vấn
+--SELECT column1, column2,...
+--FROM table_name
+--ORDER BY column1, column2,... ASC|DESC;
+--ASC: sắp xếp tăng dần mặc định không ghi
+--DESC: sắp xếp giảm dần
+
+USE [NORTHWND];
+GO
+
+--Liệt kê tất cả các nhà cung cấp theo thứ tự tên đơn vị CompanyName Suppliers
+SELECT * FROM dbo.Suppliers ORDER BY CompanyName;
+GO
+
+--Liệt kê tất cả các sản phẩm theo thứ tự giá giảm dần Products
+SELECT * FROM dbo.Products ORDER BY UnitPrice DESC;
+GO
+
+--Liệt kê tất cả các nhân viên theo thứ tự họ và tên đệm A-Z không dùng ASC|DESC Employees
+SELECT *  FROM dbo.Employees ORDER BY LastName, FirstName;
+GO
+
+--Lấy ra một sản phẩm có số lượng bán cao nhất từ bảng Order Details không được dùng MAX
+SELECT TOP 1 *  FROM dbo.[Order Details] ORDER BY Quantity;
+GO
+
+--Bài 1: Liệt kê danh sách các đơn đặt hàng (OrderID) trong bản Orders theo thú tự giảm dần của ngày đặt hàng (OrderDate)
+SELECT OrderID FROM dbo.Orders ORDER BY OrderDate;
+GO
+
+--Bài 2: Liệt kê tên, đơn giá, số lượng tồn kho (UnitsInStock) của tất cả các sản phẩm trong bảng Products theo thứ tự giảm dần của UnitsInStock
+SELECT ProductName, UnitPrice, UnitsInStock FROM dbo.Products ORDER BY UnitsInStock;
+GO
+
+
+
+
+
+
---
+++ 11.Các phép toán cộng, trừ, nhân, chia, chia lây dư .sql
@@ -0,0 +1,32 @@
+﻿--Bài 11: Các phép toán cộng, trừ, nhân, chia, chia lấy dư
+
+
+USE [NORTHWND];
+GO
+
+
+--Tính số lượng sản phẩm còn lại trong kho (UnitsInStock) sau khi bán hết các sản phẩm đã được đặt hàng (UnitsOnStock)
+-- StockReamining = UnitsInStock - UnitsOnOrder
+SELECT ProductID, ProductName, UnitsInStock, UnitsOnOrder, (UnitsInStock - UnitsOnOrder) AS [StockRemaining] FROM dbo.Products;
+GO
+
+--Tính giá trị đơn hàng chi tiết cho tất cả các sản phẩm trong bảng OrderDetails
+-- OrderDetailValue = UnitPrice x Quantity
+SELECT UnitPrice, Quantity, (UnitPrice * Quantity) AS [OrderDetailValue] FROM dbo.[Order Details];
+GO
+
+--Tính tỉ lệ giá vận chuyển đơn đặt hàng (Freight) trung bình của các đơn hàng trong bảng Orders so với giá trị vận chuyển của đơn hàng lớn nhất (MaxFreight)
+--FreightRatio = AVG(Freight)/Max(Freight)
+SELECT AVG(Freight)/MAX(Freight) AS [FreightRatio] FROM dbo.Orders;
+GO
+
+--Liệt kê danh sach các sản phẩm và giá UniPrice của từng sản phẩm sẽ được giảm giá 10%
+SELECT ProductName, UnitPrice, (UnitPrice * 0.9) AS [Giảm giá] FROM dbo.Products;
+GO
+
+
+
+
+
+
+
