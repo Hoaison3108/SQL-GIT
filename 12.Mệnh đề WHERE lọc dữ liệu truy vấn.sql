8b5d4b0d377070ef807bbc871f531e7b0fdf419e
Hoaison3108 <hoaison3108@gmail.com>
2025-06-09 16:57:45 +0700

    Update 3

---
+++ 12.Mệnh đề WHERE lọc dữ liệu truy vấn.sql
@@ -0,0 +1,72 @@
+﻿-- 12. Mệnh Đề WHERE
+-- Lọc dữ liệu trong truy vấn
+
+-- SELECT column1, column2....
+-- FROM table_name
+-- WHERE codition;
+-- Mệnh đề WHERE được sử dụng để lọc các bản ghi, được sử dụng để trích các bản ghi đáp ứng một điều kiện cụ thể
+
+USE [NORTHWND];
+GO
+
+-- Liệt kê tất cả các nhân viên đến từ thành phố London 
+SELECT *
+FROM [dbo].[Employees]
+WHERE [City] = 'London';
+GO
+
+-- Liệt kê tất cả các nhân viên đến từ thành phố London, sắp xếp kết quả theo class name AZ 
+SELECT *
+FROM [dbo].[Employees]
+WHERE [City] = 'London'
+ORDER BY [LastName] ASC;
+GO
+
+-- liệt kê các đơn hàng bị giao muộn biết rằng ngày cần giao hàng là [RequiredDate] ngày giao thực tế [ShippedDate]
+SELECT  *
+FROM [dbo].[Orders]
+WHERE [RequiredDate] < [ShippedDate];
+GO
+
+-- Đếm số lượng đơn muộn
+SELECT  COUNT (*) AS [COUNT]
+FROM [dbo].[Orders]
+WHERE [RequiredDate] < [ShippedDate];
+GO
+
+-- Liệt kê các đơn hàng được giảm giá hơn 10%
+SELECT * 
+FROM [dbo].[Order Details]
+WHERE [Discount] > 0.1;
+GO
+
+-- Liệt kê các đơn hàng được gửi từ quốc gia "France"
+SELECT *
+FROM [dbo].[Orders]
+WHERE [ShipCountry] = 'France';
+GO
+
+-- Liệt kê các sản phẩm có số lượng trong kho hơn 20
+SELECT *
+FROM [dbo].[Products]
+WHERE [UnitsInStock] > 20;
+GO
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ 13.Toán tử AND OR NOT.sql
@@ -0,0 +1,48 @@
+﻿-- 13. Toán tủ AND, OR, NOT
+-- Toán tử điều kiện
+
+-- AND/ OR
+-- SELECT column1, column2,...
+-- FROM table_name
+-- WHERE condition1 AND/OR condition2 AND/OR condition3
+
+-- NOT
+-- SELECT column1, column2,...
+-- FROM table_name
+-- WHERE NOT condition;
+-- Hiển thị một bản ghi nếu điều kiện có giá trị không đúng - FALSE
+
+USE [NORTHWND];
+GO
+
+-- Liệt kê tất cả các sản phẩm có số lượng trong kho [UnitsInStock] nhỏ hơn 50 hoặc lớn hơn 100
+SELECT *
+FROM [dbo].[Products]
+WHERE [UnitsInStock] < 50 OR [UnitsInStock] > 100;
+GO
+
+-- Liệt kê các đơn hàng được giao đến brazil đã bị muộn, biết ngày phải giao hàng là [RequiredDate] ngày giao thực tế là [ShippedDate]
+SELECT * 
+FROM [dbo].[Orders]
+WHERE [ShipCountry] = 'Brazil' AND ([RequiredDate] < [ShippedDate]);
+GO
+
+-- Lấy ra các sản phẩm cso giá dưới 100$ và mã thể loại khác 1 dùng NOT
+SELECT * 
+FROM [dbo].[Products]
+WHERE NOT [UnitPrice] > 100 AND [CategoryID] != 1; 
+GO
+
+-- Liệt kê các đơn hàng có giá vận chuyển [Freight] trong khoản [50, 100]
+SELECT * 
+FROM [dbo].[Orders]
+WHERE [Freight] > 50 AND [Freight] < 100;
+GO
+
+-- Liệt kê các sản phảm có số tồn kho [UnitsInStock] > 20 và [UnitsOnOrder] < 20
+SELECT *
+FROM [dbo].[Products]
+WHERE [UnitsInStock] > 20 AND [UnitsOnOrder] < 20;
+GO
+
+
